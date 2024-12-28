b9613335cbe357bfd7ecc6c0e099a92922fd07fa
Arshad Ali <969673arshadali@gmail.com>
2024-12-28 15:11:10 +0530

    have made the configuration such as cloudinary and mongodb and also the server where all things should be done

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,29 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+const database = require("./config/mongodb");
+const { cloudinaryConnect } = require("./config/cloudinary");
 
 const app=express()
+const PORT = process.env.PORT || 3001;
+database.connect();
+cloudinaryConnect();
+
+
+app.use(express.json());
+app.use(cors())
+
+// Testing the server
+app.get("/", (req, res) => {
+	return res.json({
+		success: true,
+		message: "Your server is up and running ...",
+	});
+});
+
+// Listening to the server
+app.listen(PORT, () => {
+	console.log(`App is listening at ${PORT}`);
+});
+
+// End of code.
