4d59f9e84a570911856811f2ed87ab1d923e90ba
Arshad Ali <969673arshadali@gmail.com>
2024-12-30 17:45:30 +0530

    add doctor and login admin done

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,7 +2,7 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from "cloudinary"
 import doctorModel from '../models/doctorModel'
-
+import jwt from 'jsonwebtoken'
 //api for adding doctor
 exports.addDoctor=async (req,res)=>{
     try{
@@ -61,4 +61,23 @@ exports.addDoctor=async (req,res)=>{
     }
 }
 
-export {addDoctor}
+//api for admin login
+const loginAdmin=async(req,res)=>{
+    try{
+        const {email,password}=req.body
+        if(email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD){
+            const token=jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }
+        else{
+            res.json({success:false,message:"invalid"})
+        }
+    }
+    catch(error){
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+
+export {addDoctor,loginAdmin}
