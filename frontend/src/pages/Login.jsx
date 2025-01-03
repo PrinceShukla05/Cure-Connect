c09f2e132a9e70851311c655ae54dad8030abb9f
Arshad Ali <969673arshadali@gmail.com>
2025-01-03 08:41:16 +0530

    have made the sign up and login controllers

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
+  const {backendUrl,token,setToken} = useContext(AppContext)
+
+  const [state, setState] = useState('Sign Up')
+  const [email,setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name,setName] = useState('')
+
+  const onSubmitHandler=async (event)=>{
+    event.preventDefault()
+    try {
+      if(state==='Sign Up'){
+        const {data}= await axios.post(backendUrl+ '/api/user/register',{name,password,email})
+        if(data.success){
+          localStorage.setItem('token',data.token)
+          setToken(data.token)
+        }
+        else{
+          toast.error(data.message)
+        }
+      }
+      else{
+        const {data}= await axios.post(backendUrl+ '/api/user/login',{password,email})
+        if(data.success){
+          localStorage.setItem('token',data.token)
+          setToken(data.token)
+        }
+        else{
+          toast.error(data.message)
+        }
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh]' flex items-center>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border'>
+        <p className='text-2xl font-semibold'>{state==='Sign Up'? "Create Accoutn":"Login"}</p>
+        <p>Please {state==='Sign Up'?"Sign Up":"Log In"} to book appointment</p>
+        {
+          state==="Sign Up" && <div className='w-full'>
+            <p>Full Name</p>
+            <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" />
+          </div>
+        }
+
+        <div className='w-full'>
+          <p>Email</p>
+          <input className='border border-zinc-300 rounded w-full py-2 mt-1' type="text" />
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input className='border border-zinc-300 rounded w-full py-2 mt-1' type="password" />
+        </div>
+        <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'></button>
+      </div>
+    </form>
   )
 }
 
