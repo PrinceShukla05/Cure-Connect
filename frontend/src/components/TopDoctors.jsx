6f309349136206348aaa1a7d864c0c2a40bbb511
Om Shukla <omshukla2403@gmail.com>
2025-01-04 15:49:56 +0530

    Updated Contact and About Page

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -13,7 +13,7 @@ import { AppContext } from '../context/AppContext'
         <p className='sm:w-1/3 self-center text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
             {doctors.slice(0, 12).map((item, index)=>(
-                <div key={index} onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-red-400 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 '>
+                <div key={index} onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}} className='border border-red-400 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 '>
                     <img className='bg-red-100' src={item.image} alt="" />
                     <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
