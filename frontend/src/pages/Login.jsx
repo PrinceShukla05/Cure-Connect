4f10997a16c49ed22c8e59daf29c1ea279a4523b
Om Shukla <omshukla2403@gmail.com>
2025-01-04 22:11:58 +0530

    Finished Login and Profile page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
   return (
     <form onSubmit={onSubmitHandler} className='min-h-[80vh]' flex items-center>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border'>
-        <p className='text-2xl font-semibold'>{state==='Sign Up'? "Create Accoutn":"Login"}</p>
+        <p className='text-2xl font-semibold'>{state==='Sign Up'? "Create Account":"Login"}</p>
         <p>Please {state==='Sign Up'?"Sign Up":"Log In"} to book appointment</p>
         {
           state==="Sign Up" && <div className='w-full'>
@@ -59,7 +59,12 @@ const Login = () => {
           <p>Password</p>
           <input className='border border-zinc-300 rounded w-full py-2 mt-1' type="password" />
         </div>
-        <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'></button>
+        <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'>{state==='Sign Up'?"Sign Up":"Log In"}</button>
+        {
+          state === "Sign Up"
+          ? <p>Already have an account? <span onClick={()=>setState('Login') } className='text-primary underline cursor-pointer'>Login here</span></p>
+          : <p>Create a new account <span onClick={()=>setState('Sign Up') } className='text-primary underline cursor-pointer'>Click here</span></p>
+        }
       </div>
     </form>
   )
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { assets } from '../assets/assets_frontend/assets'
 
-const Profile = () => {
+const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name:"Edward Vincent",
+    image: assets.profile_pic,
+    email: 'richardjames@gmail.com',
+    phone: '+1 123 456 789',
+    addfress:{
+      line1: "57th cross, Richmond",
+      line2:"Circle, Church Road, London"
+    },
+    gender:'Male',
+    dob:'2000-01-2'
+    
+  })
+
+  const [isEdit, setIsEdit] = useState(false)
+
   return (
-    <div>Profile</div>
+    <div>
+
+      <img src={userData.image} alt="" />
+
+      {
+        isEdit? <input type='text' onChange={e => setUserData(prev => ({...prev, name:e.target.value}))} />
+        : <div></div>
+}
+
+
+    </div>
   )
 }
 
-export default Profile
+export default MyProfile
